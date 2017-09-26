c60e3631c76ac96a7a2aebb9857d3598de0167a9
unknown <m362li@ECE-RTOS14.NEXUS.UWATERLOO.CA>
2017-09-26 16:24:54 -0400

    In-lab work; started some code development for Lab 1 in LAB 2 DIRECTORY under file rt_Task_ext.c

--- lab2/starter/RTX_CM3/SRC/CM/rt_Task_ext.c
+++ lab2/starter/RTX_CM3/SRC/CM/rt_Task_ext.c
@@ -22,23 +22,39 @@
 /*----------------------------------------------------------------------------
  *      Global Variables
  *---------------------------------------------------------------------------*/
-
+P_TCB p_tcb;
 
 /*----------------------------------------------------------------------------
  *      Local Functions
  *---------------------------------------------------------------------------*/
+
+int os_tsk_count_get (void);
+
 /*--------------------------- rt_tsk_count_get ------------------------------*/
 /* added in ECE254 lab keil_rtx */
 int rt_tsk_count_get (void) {
-	/* Add your own code here. Change the following line accordingly */
-	return 0;
+	int k = 0;
+	int i;
+
+	for (i = 0; i <=10; i++) {
+	if (os_active_TCB[i-1] != NULL) {
+		k++;
+	}
+	}
+	if (os_idle_TCB.task_id != 255) {
+		k++;
+	}
+	
+	return k;
 }
 
 /*--------------------------- rt_tsk_get ------------------------------------*/
 /* added in ECE254 lab keil_proc */
 OS_RESULT rt_tsk_get (OS_TID task_id, RL_TASK_INFO *p_task_info) {
+  p_tcb =&os_idle_TCB;
+  p_tcb=os_active_TCB[task_id-1];
 	/* Add your own code here. Change the following lines accordingly */
-	p_task_info->task_id     = task_id;
+	p_task_info->task_id     = p_tcb->task_id;
 	p_task_info->state       = INACTIVE;
 	p_task_info->prio        = 99;
 	p_task_info->stack_usage = 15;
